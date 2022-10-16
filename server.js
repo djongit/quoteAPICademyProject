efbcdb06f9d3d704116ee0638226e92f76653d7d
unknown <djongit@hotmail.com>
2022-10-16 17:07:21 +0100

    created POST route to add new quotes

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.get('/api/quotes/random', (req, res, next) => {
-    // console.log(req.params);
     const randomQuote = (getRandomElement(quotes));
     if(randomQuote) {
         res.send({quote: randomQuote});
@@ -19,8 +18,8 @@ app.get('/api/quotes/random', (req, res, next) => {
 })
 
 app.get('/api/quotes', (req, res, next) => {
-    console.log(req);
-console.log(req.query.person);
+//     console.log(req);
+// console.log(req.query.person);
 // console.log(res);
 if(req.query.person !== undefined) {
  const personQuotes = quotes.filter(quote => quote.person.toLowerCase() === req.query.person.toLowerCase());
@@ -32,6 +31,17 @@ if(req.query.person !== undefined) {
 
 });
 
+app.post('/api/quotes', (req, res, next)=> {
+    // console.log(req.query);
+    
+        if(req.query.quote !== '' && req.query.person !== '') {
+            quotes.push(req.query);
+            res.status(201).send({quote: req.query})
+        } else {
+            res.status(400).send('Please quote.')
+        }
+    
+});
 
 
 app.listen(PORT, ()=>{
